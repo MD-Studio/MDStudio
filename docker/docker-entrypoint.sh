6e22e053e47204acb7a445eb51df0ceb074634cc
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-04 15:35:33 +0100

    Echo build script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 echo "source /app/lie_venv/bin/activate" >> ~/.bashrc
 
 if [ -d /app/.pycharm_helpers/pydev/ ]; then
-    python /app/.pycharm_helpers/pydev/setup_cython.py build_ext --inplace  > nul
+    python /app/.pycharm_helpers/pydev/setup_cython.py build_ext --inplace &>> docker/.INSTALLING
 fi
 
 # make sure we have the bindings for our IDE
