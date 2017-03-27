aa0dc4108f28102a3f8b5c638cfdfdb5ee0528ee
p.e.visscher <p.e.visscher@zefiros.eu>
2017-03-27 11:28:15 +0200

    Test runner working

--- tests/test.py
+++ tests/test.py
@@ -23,20 +23,29 @@ def list_components_tests(search_path):
 def load_studio_tests(suite, loader, dir, type):
     if os.path.isdir(os.path.realpath(os.path.join(dir, type))):
         start_dir = os.path.realpath(os.path.join(dir, type))
-        top_level_dir = os.path.realpath(os.path.join(dir, '../../'))
-        sys.path.append(os.path.realpath(dir))
+        top_level_dir = os.path.realpath(os.path.join(dir, '../'))
         tests = loader.discover(start_dir, "*.py", top_level_dir)
         suite.addTests(tests)
+
+#def main():
+search_path = os.path.join(__root_path__, '../components/')
+
+for g in glob(os.path.join(search_path, '*/')):
+    sys.path.append(os.path.realpath(os.path.join(g, '../')))
 
 
 # Add all tests.
 alltests = unittest2.TestSuite()
-for name, path in list_components_tests(os.path.join(__root_path__, '../components/')).items():
+for name, path in list_components_tests(search_path).items():
     print("Loading '{}' from '{}'".format(name, path))
-    if name == "lie_user_tests":  # or name == "lie_config.tests":
+    if name == "lie_user_tests" or name == "lie_config_tests":
         loader = unittest2.TestLoader()
 
         load_studio_tests(alltests, loader, path, "tests/module/")
         # loadTests(alltests, loader, path, "wamp")
 
 result = unittest2.TextTestRunner(verbosity=2).run(alltests)
+
+
+#if __name__ == '__main__':
+#    main()
