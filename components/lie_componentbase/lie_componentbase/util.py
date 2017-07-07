567360e714c83f500b7e965a6511c68b5387818f
m.vanduijn <m.vanduijn@zefiros.eu>
2017-07-07 18:28:46 +0200

    Fixed componentbase

--- components/lie_componentbase/lie_componentbase/util.py
+++ components/lie_componentbase/lie_componentbase/util.py
@@ -1,6 +1,8 @@
+import inspect
 import json
 import sys
 import os
+import re
 
 from twisted.internet.defer import TimeoutError, inlineCallbacks, returnValue
 from twisted.python.failure import Failure
@@ -91,68 +93,3 @@ def extend_with_default(validator_class):
     )
 
 DefaultValidatingDraft4Validator = extend_with_default(jsonschema.Draft4Validator)
-
-def wamp_schema_handler(session):
-    def handler(uri):
-        @inlineCallbacks
-        def resolve(uri):
-            module_name = session.__module__.split('.')[0]
-            schema_path_match = re.match('wamp://liestudio\.([a-z]+)\.schemas/(.+)', uri)
-            if not schema_path_match:
-                session.log.error("Not a proper wamp uri")
-                
-            # schema_path_groups = schema_path_groups.groups()
-            schema_path = schema_path_match.group(2)
-            
-            if 'lie_{}'.format(schema_path_match.group(1)) == module_name:
-                res = yield session.get_schema(schema_path)
-            elif 'lie_{}'.format(schema_path_match.group(1)) == 'lie_system':
-                res = yield session.get_schema(schema_path, os.path.dirname(inspect.getfile(LieApplicationSession)))
-            else:
-                res = yield session.call(u'liestudio.{}.schemas', schema_path)
-
-            if res is None:
-                self.log.warn('WARNING: could not retrieve a valid schema')
-                res = {}
-            
-            returnValue(res)
-
-        return block_on(resolve(uri))
-
-    return handler
-
-def validate_json_schema(session, schema_def, request):
-    schema = {'$ref': schema_def} if type(schema_def) in (str, unicode) else schema_def
-    
-    resolver = jsonschema.RefResolver.from_schema(schema, handlers={'wamp': wamp_schema_handler(session)})
-    validator=DefaultValidatingDraft4Validator(schema, resolver=resolver)
-    
-    valid = True
-    try:
-        validator.validate(request)
-    except jsonschema.ValidationError as e:
-        session.log.error(e.message)
-        valid = False
-
-    return valid
-    
-def wamp_register(uri, input_schema, output_schema, options=None):
-    def wrap_f(f):
-        @wamp.register(uri, options)
-        @inlineCallbacks
-        def wrapped_f(self, request, **kwargs):
-            self.log.info('DEBUG: validating input with schema {}'.format(input_schema))
-            if not validate_json_schema(self, input_schema, request):
-                res = yield {}
-                returnValue(res)
-
-            res = yield f(self, request, **kwargs)
-        
-            self.log.debug('DEBUG: validating output with schema {}'.format(output_schema))
-            validate_json_schema(self, output_schema, res)
-            
-            returnValue(res)
-
-        return wrapped_f
-    
-    return wrap_f
