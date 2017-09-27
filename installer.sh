ef6d23c6e07774cc9ca7a702af5cff43bfb74ab7
felipeZ <tifonzafel@gmail.com>
2017-09-27 15:48:04 +0200

    fixed bug in the installer script

--- installer.sh
+++ installer.sh
@@ -459,7 +459,7 @@ if [[ $SETUP -eq 1 && $NO_VENV -eq 0 ]]; then
      
     # 3.1) Install Python or Anaconda/Miniconda virtual environment
     if [[ $USE_CONDA -eq 0 ]]; then
-        _setup_python_venv
+        _setup_venv
     else
         _setup_conda_venv
     fi
