dcb7d761797288aa2e2e360692d82d3b3c6a12ca
Marc van Dijk <marcvdijk@gmail.com>
2017-11-14 15:37:57 +0100

    Move LIE wamp workflow to scripts directory

--- components/lie_workflow/tests/wamp_workflow_helpers.py
+++
@@ -1,6 +0,0 @@
-def get_docking_medians(session=None, **kwargs):
-
-    medians = [v.get('path') for v in kwargs.get('output', {}).values() if v.get('mean', True)]
-    session['status'] = 'completed'
-    
-    return {'medians': medians, 'session':session}
