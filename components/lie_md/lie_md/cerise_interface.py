c6d6fa099b0ace98eec208edbbd1f2b4df388bcb
felipeZ <tifonzafel@gmail.com>
2018-02-07 16:47:46 +0100

    request energy_edr from cwl workflow

--- components/lie_md/lie_md/cerise_interface.py
+++ components/lie_md/lie_md/cerise_interface.py
@@ -394,7 +394,7 @@ def get_output(job, config):
         "gromacslog7": "{}_{}.out",
         "gromacslog8": "{}_{}.out",
         "gromacslog9": "{}_{}.out",
-        "energy":  "{}_{}.edr",
+        "energy_edr":  "{}_{}.edr",
         "energy_dataframe": "{}_{}.ene",
         "energyout": "{}_{}.out",
         "energyerr": "{}_{}.err",
