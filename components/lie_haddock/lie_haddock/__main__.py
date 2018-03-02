415e51b55d59202d5ce0d82d99ee23c52bc069e0
felipeZ <tifonzafel@gmail.com>
2018-03-02 17:28:09 +0100

    addapting Haddock #104

---
+++ components/lie_haddock/lie_haddock/__main__.py
@@ -0,0 +1,6 @@
+from mdstudio.runner import main
+from lie_haddock.wamp_services import HaddockWampApi
+
+
+if __name__ == '__main__':
+    main(HaddockWampApi)
--- components/lie_haddock/lie_haddock/wamp_services.py
+++ components/lie_haddock/lie_haddock/wamp_services.py
@@ -6,12 +6,11 @@ file: wamp_services.py
 WAMP service methods the module exposes.
 """
 
-from autobahn import wamp
+from mdstudio.api.endpoint import endpoint
+from mdstudio.component.session import ComponentSession
 
-from lie_system import LieApplicationSession
 
-
-class HaddockWampApi(LieApplicationSession):
+class HaddockWampApi(ComponentSession):
     """
     HADDOCK docking WAMP methods.
 
@@ -19,59 +18,30 @@ class HaddockWampApi(LieApplicationSession):
     upon WAMP session setup
     """
 
-    require_config = ['system', 'lie_db']
-
-    @wamp.register(u'liestudio.haddock_docking.get')
-    def retrieve_haddock_project(self, project_name, session=None):
+    @endpoint(u'get')
+    def retrieve_haddock_project(self, request, claims):
         """
         Retrieve the docking results as tar zipped archive
 
-        :param project_name: HADDOCK web server project name
-        :type project_name:  :py:str
+        :param request: HADDOCK web server project name
+        :type request:  :py:str
         """
 
-    @wamp.register(u'liestudio.haddock_docking.status')
-    def haddock_project_status(self, project_name, session=None):
+    @endpoint(u'status')
+    def haddock_project_status(self, request, claims):
         """
         Retrieve the status of a submitted haddock project
 
-        :param project_name: HADDOCK web server project name
-        :type project_name:  :py:str
+        :param request: HADDOCK web server project name
+        :type request:  :py:str
         """
 
-    @wamp.register(u'liestudio.haddock_docking.submit')
-    def submit_haddock_project(self, project_name, session=None):
+    @endpoint(u'submit')
+    def submit_haddock_project(self, request, claims):
         """
         Submit a docking project to the HADDOCK web server
 
-        :param project_name: HADDOCK web server project name
-        :type project_name:  :py:str
+        :param request: HADDOCK web server project name
+        :type request:  :py:str
         """
         pass
-
-
-def make(config):
-    """
-    Component factory
-
-    This component factory creates instances of the application component
-    to run.
-
-    The function will get called either during development using an
-    ApplicationRunner, or as a plugin hosted in a WAMPlet container such as
-    a Crossbar.io worker.
-    The BaseApplicationSession class is initiated with an instance of the
-    ComponentConfig class by default but any class specific keyword arguments
-    can be consument as well to populate the class session_config and
-    package_config dictionaries.
-
-    :param config: Autobahn ComponentConfig object
-    """
-    SETTINGS = None
-
-    if config:
-        return HaddockWampApi(config, package_config=SETTINGS)
-    else:
-        # if no config given, return a description of this WAMPlet ..
-        return {'label': 'LIEStudio HADDOCK docking web server WAMPlet',
-                'description': 'WAMPlet proving access to the HADDOCK dockign web server'}
