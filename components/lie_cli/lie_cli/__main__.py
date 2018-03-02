89ce218bae85ea68f7309a89901c22e03b72979e
felipeZ <tifonzafel@gmail.com>
2018-03-02 17:02:13 +0100

    changing command line to new API #84

--- components/lie_cli/lie_cli/__main__.py
+++ components/lie_cli/lie_cli/__main__.py
@@ -1,11 +1,13 @@
 from twisted.conch.stdio import ConsoleManhole, runWithProtocol as console
 
+
 class ConsoleWrapper(ConsoleManhole):
     def connectionMade(self):
         super(ConsoleWrapper, self).connectionMade()
         self.interpreter.push(b'from lie_cli import *')
         self.interpreter.push(b'session = connect()')
         self.lineReceived(b'print("welcome")')
+
 
 if __name__ == '__main__':
     console(ConsoleWrapper)
