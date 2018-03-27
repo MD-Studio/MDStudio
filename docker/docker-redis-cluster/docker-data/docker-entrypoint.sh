daf8611fae81479d5b3ec62c25df4d9a1dc9cf38
p.e.visscher <p.e.visscher@zefiros.eu>
2018-03-27 19:40:17 +0200

    Fix #111

--- docker/docker-redis-cluster/docker-data/docker-entrypoint.sh
+++ docker/docker-redis-cluster/docker-data/docker-entrypoint.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
 if [ "$1" = 'redis-cluster' ]; then
-    for port in `seq 7010 7017`; do
+    max_port=7007
+    if [ "$CLUSTER_ONLY" = "true" ]; then
+      max_port=7005
+    fi
+
+    for port in `seq 7000 $max_port`; do
       mkdir -p /redis-conf/${port}
       mkdir -p /redis-data/${port}
 
       if [ -e /redis-data/${port}/nodes.conf ]; then
         rm /redis-data/${port}/nodes.conf
       fi
+
+      if [ "$port" -lt "7006" ]; then
+        PORT=${port} envsubst < /redis-conf/redis-cluster.tmpl > /redis-conf/${port}/redis.conf
+      else
+        PORT=${port} envsubst < /redis-conf/redis.tmpl > /redis-conf/${port}/redis.conf
+      fi
     done
 
-    for port in `seq 7010 7015`; do
-      PORT=${port} envsubst < /redis-conf/redis-cluster.tmpl > /redis-conf/${port}/redis.conf
-    done
-
-    for port in `seq 7016 7017`; do
-      PORT=${port} envsubst < /redis-conf/redis.tmpl > /redis-conf/${port}/redis.conf
-    done
+    bash /generate-supervisor-conf.sh $max_port > /etc/supervisor/supervisord.conf
 
     supervisord -c /etc/supervisor/supervisord.conf
     sleep 3
 
-    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 7010 7011 7012 7013 7014 7015
+    # IP=0.0.0.0 # disable IP binding to bind to the host that is requested
+    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 1 7000 7001 7002 7003 7004 7005
     tail -f /var/log/supervisor/redis*.log
 else
   exec "$@"
