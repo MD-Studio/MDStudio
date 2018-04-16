730c9bab21f1f6db3569b6171643f2559e13cae2
Marc van Dijk <marcvdijk@gmail.com>
2018-04-16 15:55:51 +0200

    Add UUID model class

---
+++ components/lie_graph/lie_graph/graph_model_classes/model_identifiers.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+file: model_identifiers.py
+
+Graph model classes for working (unique) identifiers
+"""
+
+import re
+import uuid
+import logging
+
+from lie_graph.graph_mixin import NodeEdgeToolsBaseClass
+from lie_graph.graph_helpers import GraphValidationError
+
+UUID_REGEX = re.compile(r'^[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}$')
+
+
+class UUID(NodeEdgeToolsBaseClass):
+
+    @staticmethod
+    def create():
+
+        return str(uuid.uuid1())
+
+    def set(self, key, value=None):
+
+        if key == self.node_value_tag:
+            if isinstance(value, (str, unicode)) and UUID_REGEX.match(value):
+                pass
+            else:
+                raise GraphValidationError('No valid UUID: {0}'.format(value), self)
+
+        self.nodes[self.nid][key] = value
