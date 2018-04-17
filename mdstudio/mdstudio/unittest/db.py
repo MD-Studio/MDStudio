b9b553b2ab237d4a24f13e04bc9c18737c57401f
PaulVisscher <p.e.visscher@zefiros.eu>
2018-04-17 13:31:16 +0200

    Fix missing assertRaisesRegex

--- mdstudio/mdstudio/unittest/db.py
+++ mdstudio/mdstudio/unittest/db.py
@@ -1,4 +1,5 @@
 import twisted.trial.unittest as trial_unit
+from unittest2.case import _AssertRaisesContext
 
 create_mock_client = False
 
@@ -13,3 +14,17 @@ class DBTestCase(trial_unit.TestCase):
         create_mock_client = False
 
         return result
+
+    def assertRaisesRegex(self, expected_exception, expected_regex,
+                          *args, **kwargs):
+        """Asserts that the message in a raised exception matches a regex.
+
+        Args:
+            expected_exception: Exception class expected to be raised.
+            expected_regex: Regex (re pattern object or string) expected
+                    to be found in error message.
+            args: Function to be called and extra positional args.
+            kwargs: Extra kwargs.
+        """
+        context = _AssertRaisesContext(expected_exception, self, expected_regex)
+        return context.handle('assertRaisesRegex', args, kwargs)
