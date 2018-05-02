bc6f64d3f96bd269d7ae72c15e846d2e51513746
felipeZ <tifonzafel@gmail.com>
2018-05-02 13:00:22 +0200

    added register components script

---
+++ register_components.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setting variable
+export MD_CONFIG_ENVIRONMENTS=dev,docker
+
+# install cinfony from github
+pip install "https://github.com/cinfony/cinfony/tarball/master#egg=cinfony-1.2" > /dev/null 2>&1
+
+# install dependencies
+for c in $( ls components ); do
+    DIR="${PWD}/components/${c}"
+    if [ -d ${DIR} ]; then
+	pip install -e ${DIR} > /dev/null 2>&1
+    fi
+done
+
+SERVICES=( "lie_amber" "lie_echo" "lie_md" "lie_plants_docking" "lie_structures" "pylie" )
+
+for x in ${SERVICES[@]};do
+    echo "Registering component: ${x}"
+    python -m $x & > /dev/null 2>&1
+done
--- stop.sh
+++ stop.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# temporal ugly hack to kill all register lie microservices
+ps axf | grep python | grep lie | awk '{print "kill -9 " $1}' | sh
+
+# stop docker components
 docker-compose down
+
