a810cdf96621976b8df96c79c7a6a6c4ad676f01
felipeZ <tifonzafel@gmail.com>
2018-05-04 16:35:10 +0200

    wait for echo component using the travis script

--- tests/test_crossbar.py
+++ tests/test_crossbar.py
@@ -2,7 +2,6 @@ from mdstudio.deferred.chainable import chainable
 from mdstudio.component.session import ComponentSession
 from mdstudio.runner import main
 from mdstudio.utc import now
-from time import sleep
 import json
 
 
@@ -27,5 +26,4 @@ class Run_md(ComponentSession):
 
 
 if __name__ == "__main__":
-    sleep(5)  # give some time to the lie_echo to register
     main(Run_md)
