57617164605cd3374d14b8ed3fa419a0bfe406bd
felipeZ <tifonzafel@gmail.com>
2018-05-17 16:32:41 +0200

    moved lie_plants_docking to its own repo

--- standalone.sh
+++ standalone.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export WORKDIR=/tmp/mdstudio
-docker-compose up -d crossbar lie_amber lie_pylie # lie_structures
+cp ${PLANTS_BIN} ${WORKDIR}/lie_plants_docking
+docker-compose up -d crossbar lie_amber lie_plants_docking  lie_pylie  # lie_structures
