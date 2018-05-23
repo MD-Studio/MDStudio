7c822079df7eb7b8b9f1932e77cb1156a82c80c5
felipeZ <tifonzafel@gmail.com>
2018-05-23 11:20:24 +0200

    start both containers and standalone services

--- standalone.sh
+++ standalone.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
 export WORKDIR=/tmp/mdstudio
+export MD_CONFIG_ENVIRONMENTS=dev,docker
+
+# Docker based Services
+SERVICES=("lie_amber" "lie_atb" "lie_plants_docking" "lie_pylie" "lie_structures")
+
+# Services install locally with pip
+STANDALONE_SERVICES=( "lie_md" )
+
+# Create temporate files
+ALL_SERVICES=(${SERVICES[@]} ${STANDALONE_SERVICES[@]})
+for s in ${ALL_SERVICES[@]}; do
+    mkdir -p ${WORKDIR}/${s}
+done
 
 # The plants executable cannot be distribute
 cp ${PLANTS_BIN} ${WORKDIR}/lie_plants_docking
 
 # start docker microservices
-docker-compose up -d crossbar lie_amber lie_atb lie_plants_docking lie_pylie  # lie_structures
+docker-compose up -d crossbar ${SERVICES[@]}
 
-# Launch services. two times lie_md in roundrobin
-SERVICES=( "lie_md" "lie_structures")
-
-for x in ${SERVICES[@]};do
+# Start standalone components locally
+for x in ${STANDALONE_SERVICES[@]};do
     pip install "https://github.com/MD-Studio/${x}/tarball/master#egg=${x}"
     echo "Registering component: ${x}"
     python -m $x & > /dev/null 2>&1
