d982cda50b82948061d9121fc42e126eab97a4a9
felipeZ <tifonzafel@gmail.com>
2018-05-23 13:41:08 +0200

    updated standalone script

--- standalone.sh
+++ standalone.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export WORKDIR=/tmp/mdstudio
-export MD_CONFIG_ENVIRONMENTS=dev,docker
 
 # Docker based Services
 SERVICES=("lie_amber" "lie_atb" "lie_plants_docking" "lie_pylie" "lie_structures")
@@ -23,7 +22,13 @@ docker-compose up -d crossbar ${SERVICES[@]}
 
 # Start standalone components locally
 for x in ${STANDALONE_SERVICES[@]};do
-    pip install "https://github.com/MD-Studio/${x}/tarball/master#egg=${x}"
-    echo "Registering component: ${x}"
-    python -m $x & > /dev/null 2>&1
+    cd ${WORKDIR}/${x}
+    echo "installing ${x} as a standalone component!"
+    if [ ! -d ${x} ]; then
+	git clone git://github.com/MD-Studio/${x}.git --single-branch
+    else
+	cd ${WORKDIR}/${x}/${x} && git pull
+    fi
+    pip install -e ${WORKDIR}/${x}/${x} > /dev/null 2>&1
+    python -u -m $x > /dev/null 2>&1 &
 done
