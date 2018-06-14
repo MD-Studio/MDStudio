d88f930af6e7c56fae5f38009abc904fe2440751
p.e.visscher <p.e.visscher@zefiros.eu>
2018-06-14 13:18:23 +0200

    Force uninstall crossbar

--- update-crossbar.sh
+++ update-crossbar.sh
@@ -1,3 +1,4 @@
 docker-compose pull
+docker-compose run --rm crossbar pipenv uninstall crossbar
 docker-compose run --rm crossbar pipenv install --skip-lock
 docker-compose run --rm workspace pipenv install --skip-lock --dev
