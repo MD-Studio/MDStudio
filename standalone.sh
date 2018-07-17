df6390083d70d3cd27ff06bf3c22a33bfdd89c74
Marc van Dijk <marcvdijk@gmail.com>
2018-07-17 18:27:31 +0200

    Export MD_CONFIG_ENVIRONMENTS

--- standalone.sh
+++ standalone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export WORKDIR=/tmp/mdstudio
+export MD_CONFIG_ENVIRONMENTS=dev,docker
 
 # Docker based Services
 SERVICES=("lie_amber" "lie_atb" "lie_plants_docking" "lie_pylie" "lie_structures" "lie_haddock")
@@ -22,9 +23,9 @@ for x in ${STANDALONE_SERVICES[@]};do
     cd ${WORKDIR}/${x}
     echo "installing ${x} as a standalone component!"
     if [ ! -d ${x} ]; then
-	git clone git://github.com/MD-Studio/${x}.git --single-branch
+      git clone git://github.com/MD-Studio/${x}.git --single-branch
     else
-	cd ${WORKDIR}/${x}/${x} && git pull
+      cd ${WORKDIR}/${x}/${x} && git pull
     fi
     pip install -e ${WORKDIR}/${x}/${x} > /dev/null 2>&1
     if [ ${x} != "lie_cli" ]; then
