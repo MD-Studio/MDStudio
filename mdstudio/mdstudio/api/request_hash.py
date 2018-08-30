322e80520cb850f987731b48d84c64fd54fcfa65
MickVanDuijn <m.vanduijn@zefiros.eu>
2018-08-30 11:18:12 +0200

    Convert request before hashing

--- mdstudio/mdstudio/api/request_hash.py
+++ mdstudio/mdstudio/api/request_hash.py
@@ -1,7 +1,10 @@
 import json
 from base64 import b64encode
 from hashlib import sha512
+import copy
 
+from mdstudio.api.converter import convert_obj_to_json
 
 def request_hash(request):
+    request = convert_obj_to_json(copy.deepcopy(request))
     return b64encode(sha512(json.dumps(request, sort_keys=True).encode('utf8')).digest()).decode('utf8')
