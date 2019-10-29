18c2b6bb5ee2e7d62d8b4a147d80728469d5eaf2
Marc van Dijk <marcvdijk@gmail.com>
2019-10-29 13:39:53 +0100

    Auto add module to sys.path

--- components/lie_echo/lie_echo/__main__.py
+++ components/lie_echo/lie_echo/__main__.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+modulepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+if modulepath not in sys.path:
+    sys.path.insert(0, modulepath)
+
 from mdstudio.runner import main
 from lie_echo.application import EchoComponent
 
